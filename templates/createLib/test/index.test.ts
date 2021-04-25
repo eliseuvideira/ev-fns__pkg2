d29c53e476ff54a59eaf7001dafe3124d1a3b256
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-25 16:54:41 -0300

    :bug: fixed template wrong test

--- templates/createLib/test/index.test.ts
+++ templates/createLib/test/index.test.ts
@@ -1,11 +1,11 @@
 describe("index", () => {
   it("greets with Hello World!", () => {
-    expect.assertions(1);
+    expect.assertions(3);
     const log = jest.fn();
     const _ = console.log;
     console.log = log;
     try {
-      import '../src/index.ts';
+      require('../src/index.ts');
       expect(log).toHaveBeenCalled();
       expect(log).toHaveBeenCalledTimes(1);
       expect(log).toHaveBeenCalledWith("Hello World 👋");
